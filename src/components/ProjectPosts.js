2a196c6ef0b55f8fe2cde91fa23cdeb14ab0c866
alisonmyers <alison.myers@sauder.ubc.ca>
2021-11-06 16:33:53 -0700

    MyLayout

--- src/components/ProjectPosts.js
+++ src/components/ProjectPosts.js
@@ -2,7 +2,7 @@ import React from "react"
 import { MDXProvider } from "@mdx-js/react"
 import { MDXRenderer } from "gatsby-plugin-mdx"
 import { Link, graphql } from "gatsby"
-import Layout from "./Layout.js"
+import Layout from "./MyLayout.js"
 
 const shortcodes = { Link } // Provide common components here
 
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import Layout from '../components/Layout'
+import Layout from '../components/MyLayout'
 import { Link, graphql } from 'gatsby'
 
 const ProjectsPage = ({ data }) => {
