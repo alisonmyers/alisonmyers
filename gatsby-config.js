c23ac1d6f3e0f330da7d230a755ec28e998c5bca
alisonmyers <alison.myers@sauder.ubc.ca>
2021-11-06 17:20:08 -0700

    update css styles

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,12 @@ module.exports = {
       options: {
         name: `projects`,
         path: `${__dirname}/src/projects`,
+      },
+
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `images`,
+        path: path.join(__dirname, `src`, `images`),
       }
     },
     "gatsby-plugin-mdx",
