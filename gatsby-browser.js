c7031b729585ad717ce04c05271be54dbf7afdf8
alisonmyers <alison.myers@sauder.ubc.ca>
2021-11-28 20:15:04 -0800

    switch to carousel

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,5 @@
-
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import 'react-awesome-slider/dist/styles.css';
 import './src/styles/global.css';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
--- src/pages/about.js
+++ src/pages/about.js
@@ -4,15 +4,17 @@ import Projects from '../components/project-cards'
 import ProjectData from '../data/MyProjects.json'
 import SimpleSlider from '../components/simple-slider'
 
+
 const AboutPage = () => {
 
 
   return (
     <Layout pageTitle="About Me">
       <p>About Me Section.</p>
-      <div>
-        <SimpleSlider></SimpleSlider>
-      </div>
+  
+      <SimpleSlider></SimpleSlider>
+      
+      <h1>Some stuff</h1>
       <Projects projectData={ProjectData.projects}/>
 
       
