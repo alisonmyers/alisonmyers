d65e1c9546d2f9d1a193ee8cb0f440010a936115
alisonmyers <alison.myers@sauder.ubc.ca>
2021-11-28 19:47:20 -0800

    simpleslider demo content

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,6 +2,7 @@ import * as React from 'react'
 import Layout from '../components/my-layout'
 import Projects from '../components/project-cards'
 import ProjectData from '../data/MyProjects.json'
+import SimpleSlider from '../components/simple-slider'
 
 const AboutPage = () => {
 
@@ -11,6 +12,8 @@ const AboutPage = () => {
       <p>About Me Section.</p>
 
       <Projects projectData={ProjectData.projects}/>
+
+      <SimpleSlider></SimpleSlider>
     
     </Layout>
   )
